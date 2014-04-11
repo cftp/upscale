1ac585c54faf4c5a0b6864c5934e8086218ea577
Simon Wheatley <simonw@codeforthepeople.com>
2014-04-11 12:36:10 +0100

    Represent

--- plugin.php
+++ plugin.php
@@ -10,6 +10,23 @@
 
 /*  Copyright 2014 Code for the People Ltd
 
+                _____________
+               /      ____   \
+         _____/       \   \   \
+        /\    \        \___\   \
+       /  \    \                \
+      /   /    /          _______\
+     /   /    /          \       /
+    /   /    /            \     /
+    \   \    \ _____    ___\   /
+     \   \    /\    \  /       \
+      \   \  /  \____\/    _____\
+       \   \/        /    /    / \
+        \           /____/    /___\
+         \                        /
+          \______________________/
+
+
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation; either version 2 of the License, or
