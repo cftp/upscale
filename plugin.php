59406bd487bbf71554047ed17c8248ff89f9011e
Scott Evans <git@scott.ee>
2014-08-05 15:44:50 +0100

    Blob scale test

--- plugin.php
+++ plugin.php
@@ -44,44 +44,89 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
 function cftp_upscale_tiny_images($file){
-    $type = explode('/',$file['type']);
+	$type = explode('/',$file['type']);
 
-    if ( $type[0] == 'image' ) {
-        $filename = $file['tmp_name'];
-        $image = wp_get_image_editor( $filename ); // Return an implementation that extends <tt>WP_Image_Editor</tt>
-        if ( ! is_wp_error( $image ) ) {
-            $size = $image->get_size();
-            $width = $size['width'];
-            $height = $size['height'];
+	if ( $type[0] == 'image' ) {
+		$filename = $file['tmp_name'];
+		$image = wp_get_image_editor( $filename ); // Return an implementation that extends <tt>WP_Image_Editor</tt>
+		if ( ! is_wp_error( $image ) ) {
+			$size = $image->get_size();
+			$width = $size['width'];
+			$height = $size['height'];
 
-            $minwidth = apply_filters( 'cftp_upscale_minimum_width', 500.0 );
-            $minheight = apply_filters( 'cftp_upscale_minimum_height', 500.0 );
-            if ( ( $width < $minwidth ) || ( $height < $minheight ) ) {
+			$minwidth = apply_filters( 'cftp_upscale_minimum_width', 500.0 );
+			$minheight = apply_filters( 'cftp_upscale_minimum_height', 500.0 );
+			if ( ( $width < $minwidth ) || ( $height < $minheight ) ) {
 
-                if ( $width < $minwidth ) {
-                    $height = cftp_dimensions_upscale( $height, $width, $minwidth );
-                    $width = $minwidth;
-                }
-                if ( $height < $minheight ) {
-                    $width = cftp_dimensions_upscale( $width, $height, $minheight );
-                    $height = $minheight;
-                }
+				if ( $width < $minwidth ) {
+					$height = cftp_dimensions_upscale( $height, $width, $minwidth );
+					$width = $minwidth;
+				}
+				if ( $height < $minheight ) {
+					$width = cftp_dimensions_upscale( $width, $height, $minheight );
+					$height = $minheight;
+				}
 
-                $thumb = new Imagick();
-                $thumb->readImage($filename);
-                $thumb->resizeImage($width,$height,Imagick::FILTER_LANCZOS,1);
-                $thumb->writeImage($filename);
-                $thumb->clear();
-                $thumb->destroy();
+				$thumb = new Imagick();
+				$thumb->readImage($filename);
+				$thumb->resizeImage($width,$height,Imagick::FILTER_LANCZOS,1);
+				$thumb->writeImage($filename);
+				$thumb->clear();
+				$thumb->destroy();
 
-            }
-        }
-    }
-    return $file;
+			}
+		}
+	}
+	return $file;
 }
 add_filter('wp_handle_upload_prefilter','cftp_upscale_tiny_images');
 
+function cftp_upscale_bits($upload_bits) {
+
+	$ext = pathinfo($upload_bits['name'], PATHINFO_EXTENSION);
+
+	if ($ext == 'jpg' || $ext == 'jpeg' || $ext == 'png') {
+
+		$image = new Imagick();
+		$image->readimageblob($upload_bits['bits']);
+		$d = $image->getImageGeometry();
+		$width = $d['width'];
+		$height = $d['height'];
+
+		$minwidth = apply_filters( 'cftp_upscale_minimum_width', 500.0 );
+		$minheight = apply_filters( 'cftp_upscale_minimum_height', 500.0 );
+		if ( ( $width < $minwidth ) || ( $height < $minheight ) ) {
+
+			if ( $width < $minwidth ) {
+				$height = cftp_dimensions_upscale( $height, $width, $minwidth );
+				$width = $minwidth;
+			}
+			if ( $height < $minheight ) {
+				$width = cftp_dimensions_upscale( $width, $height, $minheight );
+				$height = $minheight;
+			}
+
+			$image->resizeImage($width,$height,Imagick::FILTER_LANCZOS,1);
+			$bits = $image->getImageBlob();
+
+			if (strlen($bits)) {
+				$da = $image->getImageGeometry();
+				$upload_bits['bits'] = $bits;
+				$upload_bits['name'] = str_replace($d['width']. 'x' .$d['height'], $da['width'] .'x'. $da['height'], $upload_bits['name']);
+			}
+
+			$image->clear();
+			$image->destroy();
+
+		}
+
+	}
+
+	return $upload_bits;
+}
+add_filter( 'wp_upload_bits', 'cftp_upscale_bits' );
+
 function cftp_dimensions_upscale( $x, $y, $new_y ) {
-    $ratio = $x/$y;
-    return $ratio*$new_y;
+	$ratio = $x/$y;
+	return $ratio*$new_y;
 }
